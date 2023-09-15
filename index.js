8606bc1800ed4f0bba048936c6232231cf97e50e
nicolecraven <craventhegreat@gmail.com>
2023-09-15 12:42:59 -0400

    update

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Your code here
+// Assuming you have a DODGER element in your HTML like this:
+// <div id="dodger"></div>
+
+const dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+  // Get the current left position of the dodger
+  const currentLeft = parseInt(dodger.style.left) || 0;
+
+  // Calculate the new left position by subtracting a value (e.g., 10 pixels) to move it left
+  const newLeft = currentLeft - 10;
+
+  // Ensure the dodger stays within the boundaries of its container (adjust as needed)
+  if (newLeft >= 0) {
+    dodger.style.left = newLeft + 'px';
+  }
+}
+
+function moveDodgerRight() {
+  // Get the current left position of the dodger
+  const currentLeft = parseInt(dodger.style.left) || 0;
+
+  // Calculate the new left position by adding a value (e.g., 10 pixels) to move it right
+  const newLeft = currentLeft + 10;
+
+  // Ensure the dodger stays within the boundaries of its container (adjust as needed)
+  if (newLeft + dodger.clientWidth <= window.innerWidth) {
+    dodger.style.left = newLeft + 'px';
+  }
+}
+
+// Example: Add event listeners to trigger the movement when certain keys are pressed
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'ArrowLeft') {
+    moveDodgerLeft();
+  } else if (e.key === 'ArrowRight') {
+    moveDodgerRight();
+  }
+});
+
